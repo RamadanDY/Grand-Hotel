5cd531a2833e4c90950222b957cecfd673ffefac
Ramadan <daudalaramadan2@gmail.com>
2024-02-03 22:53:42 +0000

    added an eventlitener to a btn ie about-us-btn to display more content about the hotel

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log(document.querySelector(".navigation-search-btn"));
+console.log(document.querySelector(".year").textContent);
+
+document.querySelector(".year").textContent = "2024";
+
+// console.log((document.querySelector(".footer-input").value = "rama"));
+
+// document.querySelector(".footer-btn").addEventListener("click", function () {
+//   console.log(document.querySelector(".footer-input").value);
+// });
+
+// const nepa = (document.querySelector(".about-us-paragraph").textContent =
+//   "name");
+// nepa.addEventListener("click", function () {
+//   const done = "the new age";
+// });
+const aboutbtn = document.querySelector(".about-us-btn");
+const textch = document.querySelector(".about-us-paragraph");
+
+aboutbtn.addEventListener("click", function () {
+  textch.textContent = "new shit bruhv";
+});

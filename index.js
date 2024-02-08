090fa634f851c057b018332448ab138310f647c6
Ramadan <daudalaramadan2@gmail.com>
2024-02-08 22:31:36 +0000

    try-04

--- index.js
+++ index.js
@@ -3,17 +3,17 @@ console.log(document.querySelector(".year").textContent);
 
 document.querySelector(".year").textContent = "2024";
 
-// console.log((document.querySelector(".footer-input").value = "rama"));
+console.log((document.querySelector(".footer-input").value = "rama"));
 
-// document.querySelector(".footer-btn").addEventListener("click", function () {
-//   console.log(document.querySelector(".footer-input").value);
-// });
-
-const nepa = (document.querySelector(".about-us-paragraph").textContent =
-  "name");
-nepa.addEventListener("click", function () {
-  const done = "the new age";
+document.querySelector(".footer-btn").addEventListener("click", function () {
+  console.log(document.querySelector(".footer-input").value);
 });
+
+// const nepa = (document.querySelector(".about-us-paragraph").textContent =
+//   "name");
+// nepa.addEventListener("click", function () {
+//   const done = "the new age";
+// });
 const aboutbtn = document.querySelector(".about-us-btn");
 const textch = document.querySelector(".about-us-paragraph");
 

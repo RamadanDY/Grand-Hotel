0249041b4162e219ec96639c5c85cb137ef7b8ec
Ramadan <daudalaramadan2@gmail.com>
2024-02-09 19:32:57 +0000

    i added i bacgriound color changing func to the about-us btn

--- index.js
+++ index.js
@@ -3,25 +3,30 @@ console.log(document.querySelector(".year").textContent);
 
 document.querySelector(".year").textContent = "2024";
 
-console.log((document.querySelector(".footer-input").value = "rama"));
+// console.log((document.querySelector(".footer-input").value = "rama"));
 
-document.querySelector(".footer-btn").addEventListener("click", function () {
-  console.log(document.querySelector(".footer-input").value);
-});
+// document.querySelector(".footer-btn").addEventListener("click", function () {
+//   console.log(document.querySelector(".footer-input").value);
+// });
 
 // const nepa = (document.querySelector(".about-us-paragraph").textContent =
 //   "name");
 // nepa.addEventListener("click", function () {
 //   const done = "the new age";
 // });
+
 const aboutbtn = document.querySelector(".about-us-btn");
 const textch = document.querySelector(".about-us-paragraph");
+const change = document.querySelector(".about-us");
 
 aboutbtn.addEventListener("click", function () {
   textch.textContent = "new shit bruhv";
+  change.style.backgroundColor = "oldlace";
 });
 
-for (i = 0; (i) => 2; i--) {
-  console.log("name is ");
-}
 //////lets move to the next js functionality i want to implement
+
+///////changing the background of the about-us when the button is clicked
+const number = Math.random();
+console.log(number);
+// document.querySelector("about-us").computedStyleMap.backgroundColor = "#60b347";
